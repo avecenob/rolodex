3a9b1fca463cf0b223b42c1c90c7c93116786e04
Arya Baskara <aryabasc@gmail.com>
2026-04-13 09:33:00 +0700

    feat: add Contact class for contact data model

---
+++ lib/data/contact.dart
@@ -0,0 +1,15 @@
+class Contact {
+  Contact({
+    required this.id,
+    required this.firstName,
+    this.middleName,
+    required this.lastName,
+    this.suffix,
+  });
+
+  final String id;
+  final String firstName;
+  final String lastName;
+  final String? middleName;
+  final String? suffix;
+}
